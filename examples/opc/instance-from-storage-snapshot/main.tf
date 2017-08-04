c1d76eddb51c4c6d8d4c3b9285fe181196cd890c
stephen_cross <stephen.cross@oracle.com>
2017-08-04 10:44:31 -0400

    add storage volume from snapshot example

---
+++ examples/opc/instance-from-storage-snapshot/main.tf
@@ -0,0 +1,31 @@
+provider "opc" {
+  version         = "~> v0.1.2"
+  user            = "${var.user}"
+  password        = "${var.password}"
+  identity_domain = "${var.domain}"
+  endpoint        = "${var.endpoint}"
+}
+
+data "opc_compute_storage_volume_snapshot" "snapshot1" {
+  name = "my-bootable-storage-volume/my-storage-volume-snapshot"
+}
+
+resource "opc_compute_storage_volume" "volume1" {
+  name             = "volume-from-storage-snapshot"
+  snapshot_id      = "${data.opc_compute_storage_volume_snapshot.snapshot1.snapshot_id}"
+  size             = "${data.opc_compute_storage_volume_snapshot.snapshot1.size}"
+  bootable         = true
+}
+
+resource "opc_compute_instance" "instance1" {
+  name  = "instance1"
+  label = "instance1"
+  shape = "oc3"
+
+  storage {
+    index  = 1
+    volume = "${opc_compute_storage_volume.volume1.name}"
+  }
+
+  boot_order = [1]
+}
