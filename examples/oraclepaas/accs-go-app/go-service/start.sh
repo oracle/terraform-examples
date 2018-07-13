4c65d3fa13d120e670157e5c9deab525069d2fc1
Stephen Cross <stephen.cross@oracle.com>
2018-07-13 07:49:42 -0400

    add accs examples for goland, ruby, and python

---
+++ examples/oraclepaas/accs-go-app/go-service/start.sh
@@ -0,0 +1,2 @@
+go get github.com/ant0ine/go-json-rest/rest
+go run rest-service.go
---
+++ examples/oraclepaas/accs-python-app/python-app/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+PORT_NUMBER = int(os.environ.get('PORT', 8084))
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+  # GET
+  def do_HEAD(self):
+        # Send response status code
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        return
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "Hello world!"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run():
+  print('starting server...')
+
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('0.0.0.0', PORT_NUMBER)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+run()
